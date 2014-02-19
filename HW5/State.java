13835711b89228d29150b85af2f6f891cce294b3
hhyuanf <stevenf@ucla.edu>
2014-02-18 18:42:47 -0800

    initial

---
+++ HW5/State.java
@@ -0,0 +1,5 @@
+interface State {
+    int size();
+    int[] current();
+    boolean swap(int i, int j);
+}
---
+++ HW5/UnSynchronizedState.java
@@ -0,0 +1,17 @@
+class UnSynchronizedState implements State {
+    private int[] value;
+    UnSynchronizedState(int[] v) { value = v;}
+
+    public int size() { return value.length; }
+
+    public int[] current() { return value; }
+
+    public boolean swap(int i, int j) {
+	if (value[i] <= 0) {
+	    return false;
+	}
+	value[i]++;
+	value[j]++;
+	return true;
+    }
+}
